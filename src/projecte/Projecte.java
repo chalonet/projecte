a5273cd48759025c1b1b026445ad5b2511d528f4
alumne <alumne@alumne-iesebre>
2016-10-27 09:05:23 +0200

    dav

--- src/projecte/Projecte.java
+++ src/projecte/Projecte.java
@@ -22,7 +22,7 @@ public class Projecte {
         int pes = 0;
         double mida = 0, salari = 0;
         String nom = null, nacionalitat = null, equip = null;
-        boolean titular = false, omplit =false;
+        boolean titular = false, omplert =false;
 
         //Variable del menú
         int opcio;
